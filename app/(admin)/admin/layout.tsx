173772555e66d3f4bac2e219403444bfd0823513
Mohisvst29 <mohmad.shiple@gmail.com>
2026-05-04 00:49:38 +0300

    fix: replace next-intl Link with next/link in admin panel

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { Link } from "@/navigation"
+import Link from "next/link"
 import { usePathname, useRouter } from "next/navigation"
 import { 
   LayoutDashboard, 
